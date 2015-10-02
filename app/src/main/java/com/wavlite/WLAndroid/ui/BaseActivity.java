71635febf4dc504ffee713a4bced8f5812e6accc
Jay Bardeleben <jay@wavlite.com>
2015-10-01 22:46:29 -0400

    Updated icon for toolbar

--- app/src/main/java/com/wavlite/WLAndroid/ui/BaseActivity.java
+++ app/src/main/java/com/wavlite/WLAndroid/ui/BaseActivity.java
@@ -4,39 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import com.wavlite.WLAndroid.R;
 
-
-
 /**
  * Created by javen on 6/26/15.
  */
 public class BaseActivity extends AppCompatActivity {
-
     private Toolbar mToolbar;
 
     protected Toolbar activateToolbar() {
-
-        if(mToolbar == null){
+        if(mToolbar == null) {
             mToolbar = (Toolbar)findViewById(R.id.app_bar);
-            if(mToolbar != null){
+            if(mToolbar != null) {
                 setSupportActionBar(mToolbar);
-                mToolbar.setLogo(R.drawable.ic_launcher_48);
-
+                mToolbar.setLogo(R.mipmap.ic_toolbar);
              }
         }
         return mToolbar;
     }
 
-    protected Toolbar activateToolbarWithHomeEnabled(){
+    protected Toolbar activateToolbarWithHomeEnabled() {
         activateToolbar();
-
-        if(mToolbar != null){
-
-
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);//returns back arrow for home in appbar
-
+        if(mToolbar != null) {
+            // returns back arrow for home in appbar
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
         return mToolbar;
     }
-
-
 }
